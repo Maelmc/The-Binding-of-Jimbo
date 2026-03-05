c9510e9ebcc194d55921ea4ae1a27f008f50df67
Maelmc <30575979+Maelmc@users.noreply.github.com>
2026-03-05 13:26:33 +0100

    Mongo Baby, Seraphim, Death's List update with the newest SMODS version

---
+++ src/objects/0_rebirth/j301_330.lua
@@ -0,0 +1,55 @@
+-- Mongo Baby
+SMODS.Joker {
+  key = "mongo_baby",
+  pos = {x = 6, y = 22},
+  config = {extra = {Xmult_multi = 3}},
+  loc_vars = function(self, info_queue, card)
+    if card.area and card.area == G.jokers then
+      local other_joker
+      for i = 1, #G.jokers.cards do
+        if G.jokers.cards[i] == card and G.jokers.cards[i + 1] and G.jokers.cards[i + 1].config.center.familiar then
+          other_joker = G.jokers.cards[i + 1]
+        end
+      end
+      local compatible = other_joker and other_joker ~= card and other_joker.config.center.blueprint_compat
+      local main_end = {
+        {
+          n = G.UIT.C,
+          config = { align = "bm", minh = 0.4 },
+          nodes = {
+            {
+              n = G.UIT.C,
+              config = { ref_table = card, align = "m", colour = compatible and mix_colours(G.C.GREEN, G.C.JOKER_GREY, 0.8) or mix_colours(G.C.RED, G.C.JOKER_GREY, 0.8), r = 0.05, padding = 0.06 },
+              nodes = {
+                { n = G.UIT.T, config = { text = ' ' .. localize('k_' .. (compatible and 'compatible' or 'incompatible')) .. ' ', colour = G.C.UI.TEXT_LIGHT, scale = 0.32 * 0.8 } },
+              }
+          }
+          }
+        }
+      }
+      return { main_end = main_end }
+  end
+  end,
+  rarity = 3,
+  cost = 10,
+  atlas = "jokers",
+  perishable_compat = true,
+  eternal_compat = true,
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+    local other_joker = nil
+    for i = 1, #G.jokers.cards do
+      if G.jokers.cards[i] == card and G.jokers.cards[i + 1] and G.jokers.cards[i + 1].config.center.familiar then
+        other_joker = G.jokers.cards[i + 1] 
+      end
+    end
+    local ret = SMODS.blueprint_effect(card, other_joker, context)
+    if ret then
+      ret.colour = G.C.TBOJ.MOD_COLOR
+    end
+    return ret
+  end,
+  in_pool = function (self, args)
+    return TBOJ.in_pool(self, args)
+  end
+}
