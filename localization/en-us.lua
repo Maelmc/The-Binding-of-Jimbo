c018f92b853f0f7453570010187a3b596c484a58
Maelmc <30575979+Maelmc@users.noreply.github.com>
2026-03-31 23:41:00 +0200

    Fixed challenge description & order

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1353,7 +1353,7 @@ return {
     v_text = {
         ch_c_tboj_daily_run = {"Play a random seed every day"},
         ch_c_tboj_daily_run2 = {"Resets at {C:attention}"..tostring(os.date("%I:%M %p", 0))},
-        ch_c_tboj_aprils_fool = {""},
+        ch_c_tboj_aprils_fool = {"{C:inactive}None :)"},
     },
   }
   
